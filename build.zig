e336f2d0d7212c688452a93c0bd17b1eb09e1a07
Arlo <62431063+arl0arg3nt@users.noreply.github.com>
2023-12-26 20:40:32 +0000

    Update build.zig

--- build.zig
+++ build.zig
@@ -1,18 +1,18 @@
-/*
-* Ziggui Project
-* 
-* v0.0.1
-* 
-* build.zig
-* 
-* 26.12.23
-* 
-* arl0arg3nt
-* 
-* GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
-* 
-* Copyright © 2007 Free Software Foundation, Inc. https://fsf.org/
-*/
+//
+// Ziggui Project
+// 
+// v0.0.1
+// 
+// build.zig
+// 
+// 26.12.23
+// 
+// arl0arg3nt
+// 
+// GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
+// 
+// Copyright © 2007 Free Software Foundation, Inc. https://fsf.org/
+//
 
 const std = @import("std");
 
